b74879da96deb739e026915350e90c91e813f4dd
Egor <egor.latvia@gmail.com>
2021-10-23 23:26:42 +0200

    Fix routing for authenticated user

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom'
 import Login from './pages/auth/Login'
 import { Header, Picker } from './components'
 import { Browse, Contribute, Visualize } from './pages'
+import { PrivateRoute } from 'react-auth-kit'
 
 const App = () => {
 	return (
@@ -10,7 +11,7 @@ const App = () => {
 			<Header/>
 			<Route path="/" component={ Browse } exact/>
 			<Route path="/browse" component={ Browse } exact/>
-			<Route path="/contribute/:photoIdUrl?" component={Contribute}/>
+			<PrivateRoute loginPath={"/login"} path="/contribute/:photoIdUrl?" component={Contribute}/>
 			<Route path="/login" component={ Login } exact/>
 			<Route path="/v" component={ Visualize } exact/>
 			<Route path="/p" component={ Picker } exact/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,113 +6,123 @@ import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
 import Button from '@mui/material/Button'
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-import { useIsAuthenticated, useSignOut } from "react-auth-kit";
+import AccountCircle from '@mui/icons-material/AccountCircle'
+import MenuItem from '@mui/material/MenuItem'
+import Menu from '@mui/material/Menu'
+import { useIsAuthenticated, useSignOut } from 'react-auth-kit'
+import { useAuthUser } from 'react-auth-kit'
+import axios from 'axios'
 
-export default function Header() {
-  const history = useHistory();
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const signOut = useSignOut();
+export default function Header () {
+	const history = useHistory()
+	const [anchorEl, setAnchorEl] = React.useState(null)
+	const [rating, setRating] = React.useState(null)
+	const signOut = useSignOut()
+	const isAuthenticated = useIsAuthenticated()
+	const auth = useAuthUser()
 
-  const isAuthenticated = useIsAuthenticated();
+	React.useEffect(() => {
+		if (isAuthenticated()) {
+			axios({
+				url: `http://localhost:8080/users/rating`,
+				method: 'GET',
+				withCredentials: true,
+			}).then(
+				({ data }) => setRating(data)
+			)
+		}
+	}, [])
 
-  const handleChange = (event) => {
-    setAuth(event.target.checked);
-  };
+	const handleMenu = (event) => {
+		setAnchorEl(event.currentTarget)
+	}
 
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+	const handleClose = () => {
+		setAnchorEl(null)
+	}
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+	const handleSignOut = (event) => {
+		handleClose()
+		signOut()
+	}
 
-  const handleSignOut = (event) => {
-    handleClose();
-    signOut();
-  };
+	const handleSignIn = (event) => {
+		handleClose()
+		history.push('/login')
+	}
 
-  const handleSignIn = (event) => {
-    handleClose();
-    history.push("/login");
-  };
+	const loginMenuItem = isAuthenticated() ? (
+		<MenuItem onClick={ handleSignOut }>Log out</MenuItem>
+	) : (
+		<MenuItem onClick={ handleSignIn }>Log in</MenuItem>
+	)
 
-  const loginMenuItem = isAuthenticated() ? (
-    <MenuItem onClick={handleSignOut}>Log out</MenuItem>
-  ) : (
-    <MenuItem onClick={handleSignIn}>Log in</MenuItem>
-  );
+	return (
+		<Box sx={ { flexGrow: 1 } }>
+			<AppBar position="static" sx={ { backgroundColor: '#212121' } }>
+				<Box sx={ { flexGrow: 1 } } mx={ 50 }>
+					<Toolbar>
+						<Typography variant="h6" component="div" sx={ { flexGrow: 1 } }>
+							Photos
+						</Typography>
 
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ backgroundColor: "#212121" }}>
-        <Box sx={{ flexGrow: 1 }} mx={50}>
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Photos
-            </Typography>
+						<Button
+							component={ Link }
+							to="/"
+							type="button"
+							variant="outlined"
+							color="inherit"
+							mr={ 5 }
+							sx={ { mr: 2 } }
+						>
+							Browse
+						</Button>
+						<Button
+							component={ Link }
+							to="/contribute"
+							type="button"
+							variant="outlined"
+							color="inherit"
+							mr={ 5 }
+						>
+							Contribute
+						</Button>
 
-            <Button
-              component={Link}
-              to="/"
-              type="button"
-              variant="outlined"
-              color="inherit"
-              mr={5}
-              sx={{ mr: 2 }}
-            >
-              Browse
-            </Button>
-            <Button
-              component={Link}
-              to="/contribute"
-              type="button"
-              variant="outlined"
-              color="inherit"
-              mr={5}
-            >
-              Contribute
-            </Button>
-
-            {auth && (
-              <Box sx={{ borderRadius: 16 }}>
-                <IconButton
-                  size="large"
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  variant="outlined"
-                  onClick={handleMenu}
-                  color="inherit"
-                >
-                  <AccountCircle />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  open={Boolean(anchorEl)}
-                  onClose={handleClose}
-                >
-                  {loginMenuItem}
-                </Menu>
-              </Box>
-            )}
-          </Toolbar>
-        </Box>
-      </AppBar>
-    </Box>
-  );
+						<Box sx={ { borderRadius: 16 } }>
+							<IconButton
+								size="large"
+								aria-label="account of current user"
+								aria-controls="menu-appbar"
+								aria-haspopup="true"
+								variant="outlined"
+								onClick={ handleMenu }
+								color="inherit"
+							>
+								<AccountCircle/>
+							</IconButton>
+							{ isAuthenticated() ? `Hi, ${ auth().login }` : '' }
+							<Menu
+								id="menu-appbar"
+								anchorEl={ anchorEl }
+								anchorOrigin={ {
+									vertical: 'top',
+									horizontal: 'right',
+								} }
+								keepMounted
+								transformOrigin={ {
+									vertical: 'top',
+									horizontal: 'right',
+								} }
+								open={ Boolean(anchorEl) }
+								onClose={ handleClose }
+							>
+								{ isAuthenticated() ? <p>Your rating: { rating }</p> : '' }
+								{ loginMenuItem }
+							</Menu>
+						</Box>
+					</Toolbar>
+				</Box>
+			</AppBar>
+		</Box>
+	)
 }
